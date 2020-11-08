c3b04f253b007e093b0966638b3c761e0ead5af2
npxrus <npxrus@yandex.com>
2020-11-08 09:30:41 +0300

    Added webpack config for development

---
+++ config/webpack.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/assets/scripts/main.js"),
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name][hash].[ext]",
+            outputPath: "assets/images",
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const common = require("./webpack.common");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html"),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+});
